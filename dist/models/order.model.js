41a0ad300ba7a94678199369b0fb3ff360106dc3
Florian <mrvdpflorian@gmail.com>
2023-03-17 11:26:33 +0100

    dist open

---
+++ dist/models/order.model.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var OrderStatusDto;
+(function (OrderStatusDto) {
+    OrderStatusDto["Pending"] = "Pending";
+    OrderStatusDto["Delivered"] = "Delivered";
+})(OrderStatusDto || (OrderStatusDto = {}));
+//# sourceMappingURL=order.model.js.map
---
+++ dist/models/stock.model.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var StockMovementType;
+(function (StockMovementType) {
+    StockMovementType["Supply"] = "Supply";
+    StockMovementType["Reserve"] = "Reserve";
+    StockMovementType["Removal"] = "Removal";
+})(StockMovementType || (StockMovementType = {}));
+//# sourceMappingURL=stock.model.js.map
